1a69ce2ba866fbfb9a8890c8a8dc6cab2828f722
Sohailaridwan <ridwan.sohaila@gmail.com>
2018-02-01 02:09:39 -0500

    testing branching

--- app/src/main/java/com/example/sohaila/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/sohaila/androidlabs/LoginActivity.java
@@ -7,6 +7,7 @@ public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // This comment is not in master
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
