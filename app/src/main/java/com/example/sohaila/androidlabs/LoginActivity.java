8dabaa4ad2ff090f29afd35f8ff07260db7ea930
Sohailaridwan <ridwan.sohaila@gmail.com>
2018-03-01 09:40:51 -0500

    lab3 complete

--- app/src/main/java/com/example/sohaila/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/sohaila/androidlabs/LoginActivity.java
@@ -1,13 +1,82 @@
 package com.example.sohaila.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        // Read from the shared preferences the value of the email
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        // get the default email from R and pass it in as default string returned by sharedPref.
+        String defaultEmail = getResources().getString(R.string.login);
+        String email = sharedPref.getString(getResources().getString(R.string.login_key), defaultEmail);
+
+        EditText username = (EditText) findViewById(R.id.editText);
+        username.setText(email);
+
+        Button login_button = (Button) findViewById(R.id.button2);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                EditText username = (EditText) findViewById(R.id.editText);
+                String key = getResources().getString(R.string.login_key);
+                String value = username.getText().toString();
+                editor.putString(key, value );
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
--- app/src/main/java/com/example/sohaila/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/sohaila/androidlabs/StartActivity.java
@@ -1,13 +1,73 @@
 package com.example.sohaila.androidlabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        Button b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+                // startActivity(intent);
+                startActivityForResult(intent, 50);
+            }
+        } );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 50 && resultCode == Activity.RESULT_OK){
+            Log.i(ACTIVITY_NAME, "Returns to StartActivity.onActivityResult");
+            String messgPassed = data.getStringExtra("Response");
+            CharSequence text = "ListItemsActivity Passed:"+messgPassed;
+            Toast toast = Toast.makeText(StartActivity.this,text,Toast.LENGTH_LONG);
+            toast.show();
+            Log.i(ACTIVITY_NAME,messgPassed);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
